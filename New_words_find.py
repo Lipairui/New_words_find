9f45e2b3ab5e013338e68893c83b729de0e05faf
Lipairui <953383269@qq.com>
2019-10-24 18:16:03 +0800

    Update New_words_find.py

--- New_words_find.py
+++ New_words_find.py
@@ -153,10 +153,10 @@ def new_words_find(sentences, min_freq=10, min_mtro=80, min_entro=3):
 #     score = sorted(score.items(),key=lambda x:x[1],reverse=True)
     return result
    
- if __name__=='__main__':
+if __name__=='__main__':
     sentences = ['小程序应该上个锁，让小朋友不能玩游戏。要不孩子天天就盯着玩','为什么微信小程序游戏点击会立刻闪退','加载小程序信息失败，无法起动',
-                '在小程序里很卡，总是死机','摇一摇摇到的怎么都是机器人','微信摇一摇摇到的人感觉都是假的，没人回也没动静','请解绑，摇一摇功能，谢谢']]]]]
-    
-    result = new_words_find(sentences, min_freq=10, min_mtro=80, min_entro=3)
+                '在小程序里很卡，总是死机','摇一摇摇到的怎么都是机器人','微信摇一摇摇到的人感觉都是假的，没人回也没动静','请解绑，摇一摇功能，谢谢',
+                '附近人摇一摇都加不上好友','无法正常试用微信摇一摇功能']
+    result = new_words_find(sentences, min_freq=3, min_mtro=1, min_entro=0.5) 
     for res in result:
-        print(res)
+        print(res[0],res[1][0])
